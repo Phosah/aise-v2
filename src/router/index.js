4bf0d0fdd9f6256de72b722a208cdc9877d7733c
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-04-22 09:41:11 -0700

    Creation of views and routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,53 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import AistakeFinance from "../views/AistakeFinance.vue";
+import Athena from "../views/Athena.vue";
+import Works from "../views/Works.vue";
+import Xerxes from "../views/Xerxes.vue";
+import Pegasus from "../views/Pegasus.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/aistakefinance",
+      name: "AistakeFinance",
+      component: AistakeFinance,
+    },
+    {
+      path: "/athena",
+      name: "Athena",
+      component: Athena,
+    },
+    {
+      path: "/works",
+      name: "Works",
+      component: Works,
+    },
+    {
+      path: "/xerxes",
+      name: "Xerxes",
+      component: Xerxes,
+    },
+    {
+      path: "/pegasus",
+      name: "Pegasus",
+      component: Pegasus,
+    },
+    {
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      component: () => import("../views/About.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;

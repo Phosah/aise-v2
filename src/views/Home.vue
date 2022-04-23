568364b472b3486d0c5f54d09200b138988174d3
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-04-23 08:32:12 -0700

    More updates to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,5 +4,43 @@
 
 <template>
   <main>
+    <div class="max-w-7xl mx-auto mt-20 flex items-center">
+      <h1 class="w-2/3 text-9xl">Product</h1>
+      <p class="w-1/3">Hello There! I am Aise, a User Experience designer focused on maximizing business need and user
+        satisfaction through the creation of simple digital experiences.</p>
+    </div>
+    <div class="max-w-7xl mx-auto flex justify-center mt-10">
+      <h1 class="text-9xl">Designer</h1>
+    </div>
+    <div class="max-w-7xl mx-auto mt-10">
+      <div class="flex justify-end">
+        <img class="w-1/2" src="../assets/aise-photo-1.png" alt="Aise photo">
+      </div>
+    </div>
+    <div class="max-w-7xl mx-auto flex">
+      <div class="w-1/2">
+        <h2 class="mb-6 text-4xl">What I do</h2>
+        <p class="pl-6 border-l-2 border-brand-black-2 border-opacity-50">Hello There! I am Aise, a User Experience
+          designer
+          focused on maximizing business need and user satisfaction through the creation of simple digital experiences.
+        </p>
+      </div>
+    </div>
+    <div class="max-w-7xl mx-auto my-20">
+      <div class="flex items-center space-x-12">
+        <div class="p-8 bg-black text-white">
+          <h2 class="text-6xl">Web 3</h2>
+          <h2 class="mb-8 text-6xl">Projects</h2>
+          <div class="mb-8">Explore</div>
+          <img src="../assets/web3-projects.png" alt="Web 3 Projects">
+        </div>
+        <div class="p-8">
+          <h2 class="text-6xl">Other</h2>
+          <h2 class="mb-8 text-6xl">Projects</h2>
+          <div class="mb-8">Explore</div>
+          <img src="../assets/other-projects.png" alt="Other Projects">
+        </div>
+      </div>
+    </div>
   </main>
 </template>

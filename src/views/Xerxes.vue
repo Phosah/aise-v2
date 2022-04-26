d63a4073037a5f0618e45f700ff750e3b65c0b0a
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-04-26 11:08:24 -0700

    More fixes to Xerxes page

--- src/views/Xerxes.vue
+++ src/views/Xerxes.vue
@@ -43,5 +43,179 @@
                 </div>
             </div>
         </section>
+        <section class="max-w-7xl mx-auto my-20">
+            <div class="flex items-center space-x-12 my-20">
+                <div class="flex-1">
+                    <h3 class="mb-6 text-3xl font-bold">Problems Faced by Users</h3>
+                    <p class="mb-4">For every crypto investor, there has been one major statement we get advised with
+                        before we being
+                        and
+                        that is “Don't put in more than you can lose”. But this is one piece of advice we know but like
+                        me,
+                        a lot of users fail to uphold.</p>
+
+                    <p class="mb-4">The frequent dips in cryptocurrencies have been that of major benefits and also
+                        losses on a lot
+                        of
+                        users which often places users in a tight spot especially those who have a dire need for their
+                        investments.</p>
+
+                    <p class="mb-4">A lot of users tend to embrace the HODL stance and keep their funds for two major
+                        reasons; The
+                        faith
+                        in the tokens to rise back based on its historical proofs and the pain of selling now and
+                        incurring
+                        all the loss.</p>
+
+                    <p>This has kept a lot of users facing serious financial problems, those who have to starve or lack
+                        some basic amenities because their funds are tied up suffer from it.</p>
+                </div>
+                <div class="flex-1"><img class="w-full" src="../assets/xerxes-photo-1.png" alt="Xerxes photo"></div>
+            </div>
+            <div class="flex items-center space-x-12 my-20">
+                <div class="flex-1"><img class="w-full" src="../assets/xerxes-photo-2.png" alt="Xerxes photo"></div>
+                <div class="flex-1">
+                    <h3 class="mb-6 text-3xl font-bold">What Does Xerxes aim to Achieve?</h3>
+                    <p class="mb-4">Crypto Investments do not need to be one that financially cripples you, the goal is
+                        financial liberation
+                        both short and long term.</p>
+
+                    <p class="mb-4">Xerxes hopes to bring a solution that allows users access to lending systems whereby
+                        you can borrow
+                        tokens either stable coins or others with the collateralization of your crypto investments and
+                        sort out
+                        whatever financial needs, you don't need to tell us without ever having to sell and be on the
+                        losing
+                        side of the dip</p>
+
+                    <p class="mb-4">Also what is a DeFi Protocol without investment opportunities, so we aim to make the
+                        system embrace the
+                        decentralized nature of web3, hence we aim to facilitate a peer-peer lending system where the
+                        borrowers
+                        would be incentivized with profit for borrowing their extra cash to those who need it in a
+                        secured
+                        nature.</p>
+                </div>
+            </div>
+        </section>
+        <section class="max-w-7xl mx-auto my-20">
+            <h1 class="mb-6 text-6xl font-bold">Research Time</h1>
+            <p class="mb-4">During the research phase, we engaged with a lot of users in the web2 space and also the
+                web3 space
+                because the goal of the project is to understand the psychology of users wanting to borrow funds and
+                leverage off lending protocols, and due to this technology being utilized in the web2 space already we
+                needed to understand from a solutions perspective. But we understand that the methods of the approach of
+                lending in web3 are different because of the peer-peer fashion we also needed to understand how similar
+                protocols in the web3 space carry out lending and borrowing.</p>
+
+            <p>After listening carefully to a couple of users in some one on one interviews scheduled on google meet,
+                and some sharing the vision session with the product owners I was able to identify some business needs
+                and at the same time identify the pain points of the users and able to come up with some deductions.</p>
+        </section>
+        <section class="max-w-7xl mx-auto my-20">
+            <h1 class="mb-6 text-6xl font-bold">Deductions</h1>
+            <p class="mb-6">The research has shown that it was crucial for the success of the project to identify the
+                painpoints and translate them into solutions</p>
+            <ul class="pl-6 list-disc">
+                <li class="mb-3">Users needed a way to borrow money without necessarily supplying first</li>
+                <li class="mb-3">Customizable Loan Terms</li>
+                <li class="mb-3">Repayments flexibility, I can decide to clear my loans once or set recurring payments
+                    connected to
+                    my wallet</li>
+                <li class="mb-3">Transparent risk assessment</li>
+                <li class="mb-3">The ability to multiple tokens from the wallet to borrow one asset on the platform</li>
+                <li class="mb-3">Repayment terms and answered questions</li>
+                <li class="mb-3"> Clarity on terms used in the lending/borrowing cycle</li>
+            </ul>
+        </section>
+        <section class="max-w-7xl mx-auto my-20">
+            <h1 class="mb-6 text-6xl font-bold">User Personas</h1>
+            <p class="mb-6">Based on the generative user research I conducted, I created user personas that capture the
+                essence of
+                both user categories so its easier to personalize the experience to addressing the users pain points</p>
+            <div class="flex items-center space-x-12 mb-20">
+                <div class="w-1/3"><img src="../assets/persona-1.png" alt="Persona"></div>
+                <div class="w-2/3">
+                    <div class="mb-6">
+                        <h3 class="mb-4 text-3xl">About</h3>
+                        <p>Wade Warren is a 26yrs old Human Resource manager who invests a lot in crypto, he isn't so
+                            experienced with trading and other forms of investments so he buys to hold long term, most
+                            of his holdings are in Ethereum and Doge</p>
+                    </div>
+                    <div class="mb-6">
+                        <h3 class="mb-4 text-3xl">Goals</h3>
+                        <ul class="pl-6 list-disc">
+                            <li>Wade needs a way to have funds to use for another personal run around and at the same
+                                time HODL his tokens because he doesn't want to lose out on any Rise</li>
+                            <li> Wade also needs a way to borrow and be sure of the terms he is getting into based on
+                                bank lending schemes he is a bit untrusting</li>
+                            <li>Wade needs a way to earn passive income with his tokens as he is holding long term</li>
+                        </ul>
+                    </div>
+                    <div class="mb-6">
+                        <h3 class="mb-4 text-3xl">Pain Points</h3>
+                        <ul class="pl-6 list-disc">
+                            <li>Not knowing how to get cash to spend so he ends up borrowing funds from friends to meet
+                                personal needs and sometimes the friends might not have the funds so he is stuck.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="flex items-center space-x-12">
+                <div class="w-1/3"><img src="../assets/persona-2.png" alt="Persona"></div>
+                <div class="w-2/3">
+                    <div class="mb-6">
+                        <h3 class="mb-4 text-3xl">About</h3>
+                        <p>Rebecca Richards is a 29yrs old Sales agent at Chipper Cash she stays in Amsterdam. She is
+                            more of a take-profit seller and holds short-term until the token has yielded a substantial
+                            profit. She controls a diversified portfolio to maximize her luck</p>
+                    </div>
+                    <div class="mb-6">
+                        <h3 class="mb-4 text-3xl">Goals</h3>
+                        <ul class="pl-6 list-disc">
+                            <li>Rebecca needs a way to borrow funds when in need of cash and at the same time not risk
+                                her chances of a potential profit</li>
+                            <li>Rebecca wants a way to borrow funds when they are high with a more stable token when she
+                                predicts the tokens will dip so she can leverage off the dip and earn profit as well
+                            </li>
+                            <li>Rebecca needs a way to earn passive income with his tokens as he is holding long term
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="mb-6">
+                        <h3 class="mb-4 text-3xl">Pain Points</h3>
+                        <ul class="pl-6 list-disc">
+                            <li>Overpriced interest placed on loans</li>
+                            <li>Often when the price of a token collateralized falls in the market your
+                                collateralization limit might cross the safe point without her being aware and she might
+                                lose her collateral due to the protocol of liquidating it to avoid loss.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="my-20">
+            <div class="max-w-7xl mx-auto">
+                <h1 class="mb-6 text-6xl font-bold">How might we</h1>
+                <p class="mb-6">At this stage I had to put together all the information I gathered so I could generate
+                    some How might we questions that guides the ideation process, which helps us to do with thinking of
+                    a solution based on the identified problem</p>
+            </div>
+            <div><img class="w-full" src="../assets/proposition.png" alt="Proposition"></div>
+        </section>
+        <section class="max-w-7xl mx-auto my-20">
+            <h1 class="mb-4 text-6xl font-bold">Value Proposition - Customers</h1>
+            <p class="mb-4">In the customer's profile, you describe the job your customer is trying to get done by
+                highlighting customers pains and gains, it's used to track users needs and position the company’s to
+                know more about the customers needs to properly create value</p>
+            <div><img class="w-full" src="../assets/value-proposition-customers.png" alt="Value Proposition"></div>
+        </section>
+        <section class="max-w-7xl mx-auto my-20">
+            <h1 class="mb-4 text-6xl font-bold">Value Proposition - Business</h1>
+            <p class="mb-4">Here you list the products and services your value proposition is built on, describing in
+                which way the
+                product features and services are pain relievers and outline how they are gain creators</p>
+            <div><img class="w-full" src="../assets/value-proposition-business.png" alt="Value Proposition"></div>
+        </section>
     </main>
 </template>

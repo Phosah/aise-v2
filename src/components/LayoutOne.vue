03b9e6f0fc551cdfc3ee2b9fb28384fa1882ad42
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-05-09 13:43:24 -0700

    Updates to mobile responsiveness

--- src/components/LayoutOne.vue
+++ src/components/LayoutOne.vue
@@ -8,12 +8,12 @@ defineProps({
 </script>
 <template>
     <div class="md:flex md:items-center md:space-x-12 md:my-40 my-20">
-        <div class="flex-1">
+        <div class="md:flex-1 mb-12 md:mb-0">
             <h3 class="font-new-paris-kingsize mb-6 text-3xl font-bold">{{ page.title }}</h3>
             <div class="mb-4" v-for="(para, ix) in page.paragraphs" :key="ix">
                 <p>{{ para }}</p>
             </div>
         </div>
-        <div class="flex-1"><img class="w-full" :src="page.img" :alt="page.alt"></div>
+        <div class="md:flex-1"><img class="w-full" :src="page.img" :alt="page.alt"></div>
     </div>
 </template>

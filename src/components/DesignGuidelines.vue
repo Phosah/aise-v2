3c5b1dced68ae1c5d61d700547719ff4cbf98808
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-06-04 05:00:37 -0700

    Addition of transition to Design Guidelines comp

--- src/components/DesignGuidelines.vue
+++ src/components/DesignGuidelines.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="mb-3 md:mb-6 px-6 py-6 bg-brand-blue-1 shadow-sm" :class="border">
+    <div class="mb-3 md:mb-6 px-6 py-6 bg-brand-blue-1 shadow-sm hover:transform hover:-translate-y-3 transition-all" :class="border">
         <div class="mb-8"><img :src="imageUrl" :alt="altText"></div>
         <h4 class="mb-6 text-2xl lg:text-3xl font-semibold font-playfair">{{ title }}</h4>
         <p>{{ text }}</p>

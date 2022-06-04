54272980e9aef88cefca7f952d3855f91bf49532
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-06-04 10:55:54 -0700

    Update

--- src/components/DesignGuidelines.vue
+++ src/components/DesignGuidelines.vue
@@ -1,34 +1,26 @@
 <template>
-    <div class="mb-3 md:mb-6 px-6 py-6 bg-brand-blue-1 shadow-sm hover:transform hover:-translate-y-3 transition-all" :class="border">
-        <div class="mb-8"><img :src="imageUrl" :alt="altText"></div>
-        <h4 class="mb-6 text-2xl lg:text-3xl font-semibold font-playfair">{{ title }}</h4>
-        <p>{{ text }}</p>
+    <div class="bg-brand-black-1 text-white">
+      <div class="max-w-7xl mx-auto px-4 md:px-20 py-20 md:py-40">
+        <h2 class="mb-20 text-4xl md:text-6xl lg:text-7xl text-white font-new-paris-kingsize-regular">MY DESIGN
+          GUIDELINES</h2>
+        <div class="grid sm:grid-cols-2 md:grid-cols-3 gap-10">
+          <template v-for="(guideline, index) in guidelines" :key="index">
+            <div class="mb-3 md:mb-6 px-6 py-6 bg-brand-blue-1 shadow-sm hover:transform hover:-translate-y-3 transition-all" :class="guideline.border">
+                <div class="mb-8"><img :src="guideline.img" :alt="guideline.alt"></div>
+                <h4 class="mb-6 text-2xl lg:text-3xl font-semibold font-playfair">{{ guideline.title }}</h4>
+                <p>{{ guideline.text }}</p>
+            </div>
+          </template>
+        </div>
+      </div>
     </div>
 </template>
-
 <script>
 export default {
     name: 'DesignGuidelines',
     props: {
-        imageUrl: {
-            type: String,
-            default: ""
-        },
-        altText: {
-            type: String,
-            default: "An alt text"
-        },
-        title: {
-            type: String,
-            default: ""
-        },
-        text: {
-            type: String,
-            default: ""
-        },
-        border: {
-            type: String,
-            default: "border-t-0 rounded-none"
+        guidelines: {
+            type: Array,
         }
     }
 }

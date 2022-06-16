b54f68092f6ccd0a2a63dae04e9aeabbae48893b
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-06-16 06:15:01 -0700

    Fixes

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,13 +1,13 @@
 <script setup></script>
 
 <template>
-    <div class="max-w-7xl mx-auto py-20 grid grid-cols-6 grid-row-6 gap-6 gap-x-10">
-        <div class=" bg-red-200 row-start-2 row-span-4"><img class="w-full h-full object-cover"  src="../assets/gallery-1.png" alt="Gallery photo"></div>
-        <div class=" bg-blue-200 col-start-2 row-start-1 row-span-3"><img class="w-full h-full object-cover"  src="../assets/gallery-2.png" alt=""></div>
-        <div class=" bg-white row-start-2 row-span-6 col-span-2"><img class="w-full h-full object-cover"  src="../assets/gallery-6.png" alt=""></div>
-        <div class=" bg-green-200 col-start-5 row-start-1 row-span-3"><img class="w-full h-full object-cover"  src="../assets/gallery-5.png" alt="Gallery photo"></div>
-        <div class=" bg-orange-200 row-start-2 row-span-4"><img class="w-full h-full object-cover"  src="../assets/gallery-7.png" alt="Gallery photo"></div>
-        <div class=" bg-purple-200 col-start-2 row-start-4 row-span-3"><img class="w-full h-full object-cover"  src="../assets/gallery-3.png" alt="Gallery photo"></div>
-        <div class=" bg-yellow-200 col-start-5 row-span-3"><img class="w-full h-full object-cover"  src="../assets/gallery-4.png" alt="Gallery photo"></div>
+    <div class="max-w-7xl mx-auto py-20 grid grid-cols-6 grid-row-6 gap-3 md:gap-6">
+        <div class="row-start-2 row-span-4"><img class="w-full h-full object-cover"  src="../assets/gallery-1.png" alt="Gallery photo"></div>
+        <div class="col-start-2 row-start-1 row-span-3"><img class="w-full h-full object-cover"  src="../assets/gallery-2.png" alt=""></div>
+        <div class="row-start-2 row-span-6 col-span-2"><img class="w-full h-full object-cover"  src="../assets/gallery-4.png" alt=""></div>
+        <div class="col-start-5 row-start-1 row-span-3"><img class="w-full h-full object-cover"  src="../assets/gallery-5.png" alt="Gallery photo"></div>
+        <div class="row-start-2 row-span-4"><img class="w-full h-full object-cover"  src="../assets/gallery-7.png" alt="Gallery photo"></div>
+        <div class="col-start-2 row-start-4 row-span-3"><img class="w-full h-full object-cover"  src="../assets/gallery-3.png" alt="Gallery photo"></div>
+        <div class="col-start-5 row-span-3"><img class="w-full h-full object-cover"  src="../assets/gallery-6.png" alt="Gallery photo"></div>
     </div>
 </template>

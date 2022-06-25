1272d0531c6ca726ac3ce0b7471b339edc1c1525
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-06-25 10:58:40 -0700

    Addition of social media icons for dark mode

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -9,19 +9,24 @@
         <a :class="mode === light ? 'text-brand-black-3 hover:border-brand-black-3' : 'text-white hover:border-white' " class="block pb-2 text-xl border-b-4 border-transparent hover:border-b-4"
             href="https://twitter.com/Aise_Idahor" target="_blank">Twitter</a>
     </div>
-    <div class="md:hidden flex items-center justify-between space-x-6 max-w-7xl mx-auto px-6 mt-20 py-3">
-        <a href=""><img src="../assets/linkedin-dark.png" alt=""></a>
-        <a href=""><img src="../assets/behance-dark.png" alt=""></a>
-        <a href=""><img src="../assets/dribbble-dark.png" alt=""></a>
-        <a href=""><img src="../assets/twitter-dark.png" alt=""></a>
+    <div :class="mode === light" class="md:hidden flex items-center justify-between space-x-6 max-w-7xl mx-auto px-6 mt-20 py-3">
+        <a href="">
+            <img v-if="mode === light" src="../assets/linkedin-dark.png" alt="">
+            <img v-else src="../assets/linkedin-white.png" alt="">
+        </a>
+        <a href="">
+            <img v-if="mode === light" src="../assets/behance-dark.png" alt="">
+            <img v-else src="../assets/behance-white.png" alt="">
+        </a>
+        <a href="">
+            <img v-if="mode === light" src="../assets/dribbble-dark.png" alt="">
+            <img v-else src="../assets/dribbble-white.png" alt="">
+        </a>
+        <a href="">
+            <img v-if="mode === light" src="../assets/twitter-dark.png" alt="">
+            <img v-else src="../assets/twitter-white.png" alt="">
+        </a>
     </div>
-    <!-- <div class="md:hidden flex items-center justify-center space-x-4 max-w-7xl mx-auto px-6 mt-20 py-3">
-        <a href=""><img src="../assets/social-media-1.jpeg" alt=""></a>
-        <a href=""><img src="../assets/social-media-2.jpeg" alt=""></a>
-        <a href=""><img src="../assets/social-media-3.jpeg" alt=""></a>
-        <a href=""><img src="../assets/social-media-4.jpeg" alt=""></a>
-        <a href=""><img src="../assets/social-media-5.jpeg" alt=""></a>
-    </div> -->
 </template>
 
 <script>

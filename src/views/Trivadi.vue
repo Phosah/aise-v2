7b666f988de0a0030a0e3d4f2dde968c3496ed12
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-07-20 17:57:24 -0700

    complete structure of Trivadi page

--- src/views/Trivadi.vue
+++ src/views/Trivadi.vue
@@ -6,8 +6,22 @@ import trivadiCover from '../assets/trivadi/trivadi-home.png'
 import background from "../assets/trivadi/background.png"
 import photo1 from '../assets/trivadi/research.png'
 import CreatePersonas from '../components/CreatePersonas.vue';
+import Footer from '../components/Footer.vue';
+import OtherProjects from '../components/OtherProjects.vue';
 import persona1 from '../assets/trivadi/persona-1.png'
 import persona2 from "../assets/trivadi/persona-2.png"
+import LayoutTwo from '../components/LayoutTwo.vue';
+import LayoutThree from '../components/LayoutThree.vue';
+import photo11 from "../assets/trivadi/account.png"
+import photo2 from "../assets/trivadi/home.png"
+import photo3 from "../assets/trivadi/discover.png"
+import photo4 from "../assets/trivadi/shipment.png"
+import photo5 from "../assets/trivadi/trip.png"
+import photo6 from "../assets/trivadi/create-trip.png"
+import photo7 from "../assets/trivadi/booking.png"
+import photo8 from "../assets/trivadi/tour.png"
+import photo9 from "../assets/trivadi/game.png"
+import photo10 from "../assets/trivadi/speech.png"
 
 const info = {
     title: "Trivadi",
@@ -69,6 +83,91 @@ const personasArr = [
         ]
     },
 ]
+const account = {
+    title: "Account Creation",
+    paragraphs: [
+        "The account creation was designed to be simple and intuitive where the user can set up his account with just his user name and profile then select his country and set his location so his initial discovery feed would be well-tailored to his locality. The user also gets to save his country of interests as this affects the discovery experiences he would see",
+    ],
+    img: photo11,
+    alt: "account"
+}
+const home = {
+    title: "Home Page",
+    paragraphs: [
+        "The home page is simple and straight to the point as it gives you access to the major thing the app has to offer. With the most prominent thing on the screen be the track shipment which is very necessary to know the current location and status of your packages being sent out",
+        "Also, you have access to other key features in the product, your shipment history, and popular travel locations"
+    ],
+    img: photo2,
+    alt: "home"
+}
+const discovery = {
+    title: "Discovery",
+    paragraphs: [
+        "The discovery page is tailored to account for all travelers needs and help travelers get the best experience in whatever country you go to",
+        "In the discovery you can get to see stores for clothes to buy in whatever location, see restaurants and filter down to your taste and, you get to see events created by people around you that you can attend, travel guides and more the goal is to help you optimize your experience,  in the country of choice, feel among and have a meaningful travel"
+    ],
+    img: photo3,
+    alt: "discover"
+}
+const shipment = {
+    title: "Create Shipment",
+    paragraphs: [
+        "The create shipment is one of the pillars of the product as shipping items to be used in the new country is one of the pain points for users, so the create shipment has been designed in such a way to ease the stress of users in figuring out the exact dimensions and packing with the right boxes, with  the use of AR we can scan ur package and estimate with 100% accuracy the size and the box dimensions so we would have the boxes readily available for shipping "
+    ],
+    img: photo4,
+    alt: "discover"
+}
+const trip = {
+    title: "My Trips",
+    paragraphs: [
+        "My trips serve as a collection of events and memories that i could capture withing the entire trip and have it properly arranged for me to access anytime"
+    ],
+    img: photo5,
+    alt: "trips"
+}
+const create = {
+    title: "Create Trip",
+    paragraphs: [
+        "The create trip is designed to help uses on the overall experience of a trip from accounting for all the relevant info or document needed for the travel to shipping of items to the country of your choice and setting up booking accommodation reminders "
+    ],
+    img: photo6,
+    alt: "create"
+}
+const booking = {
+    title: "Booking",
+    paragraphs: [
+        "Booking accommodation is split into two parts, one is hotels and which are usually for short-time stayers and the second is apartments and this is for travelers who would stay longer and the renting of an apartment would provide cheaper accommodation. So on Trivadi you get to see varieties of apartments, their amenities, and this can be seen in AR for better experience"
+    ],
+    img: photo7,
+    alt: "trips"
+}
+const tour = {
+    title: "Tour Guide",
+    paragraphs: [
+        "The tour guide feature is designed to help travelers who would love to explore the city, especially one, not familiar to them in conditions like language, culture, etc and this guides can be selected on the platform and you can see their reviews, about the, their likes so you can pick who relates a bit to you as you most likely would spend some time with them"
+    ],
+    img: photo8,
+    alt: "tour"
+}
+const task = {
+    title: "Gamified Tasks",
+    paragraphs: [
+        "Most travelers during trips set out to do things they want to accomplish, and from some interviews, these are not always completed, occasionally due to procrastination and sometimes laziness.",
+        "So the gamified tasks help users set up tasks with spouses or friends that are traveling together and for every task completed you earn points that can be used for shopping, apartment renting, or food purchase on the app"
+    ],
+    img: photo9,
+    alt: "Gamified Tasks"
+}
+const speech = {
+    title: "Speech Translator & Translator From Image",
+    paragraphs: [
+        "The translator solution is designed for travelers going to countries with languages different from the local languages they are used to. This helps them communicate with locals in such countries so they can have the maximum experience of the travel",
+        "So most travelers especially in countries in countries that they do not understand their language relate with a lot of things, not just the people who speak, these things can range from signposts to restaurant menus, etc. This feature allows users to be able to scan an image and the app helps highlight the texts and translates them to your desired language. Due to the fact that the image scanner might not capture all the words so you can always edit the text of the scanned words"
+    ],
+    img: photo10,
+    alt: "Gamified Tasks"
+}
+const themeMode = "light"
 </script>
 
 <template>
@@ -150,5 +249,27 @@ const personasArr = [
             </div>
             <CreatePersonas :personas="personasArr" />
         </section>
+        <section class="my-20 px-6">
+            <LayoutOne :page="account" />
+            <LayoutTwo :page="home" />
+            <LayoutOne :page="discovery" />
+            <LayoutTwo :page="shipment" />
+            <LayoutOne :page="trip" />
+            <LayoutTwo :page="create" />
+            <LayoutOne :page="booking" />
+            <LayoutTwo :page="tour" />
+            <LayoutOne :page="task" />
+            <LayoutThree :page="speech" />
+        </section>
+         <section class="max-w-7xl mx-auto my-20 md:my-40 px-6">
+            <h1 class="mb-12 text-2xl md:text-5xl font-bold font-new-paris-kingsize">Key Takeaways</h1>
+            <div class="mb-12">
+                <h2 class="mb-6 text-2xl font-bold font-new-paris-kingsize">Learnings</h2>
+                <p class="mb-6">The project was fun to work on and I took my time to understand how travelers think and operate it made me properly identify the pain points of users and think of possible solutions both that could help their pain points and also provide an extra solution to provide more value</p>
+                <p class="bg-red-300">Thank you</p>
+            </div>
+        </section>
+        <OtherProjects />
+        <Footer :bgColor="themeMode"/>
     </main>   
 </template>

75df432a01f96ce311c3682bdbffe93debc2098f
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-07-23 15:36:01 -0700

    fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -21,14 +21,14 @@
             </div>
             <div class="md:flex md:items-center md:justify-center md:space-x-8 w-full">
                 <a href="mailto://aiseosauyiidahor@gmail.com">
-                    <div :class="mode === light ? 'border-brand-black-3 hover:border-white bg:border-brand-black-3' : 'border-white hover:border-brand-black-3 hover:bg-white hover:text-brand-black-1'"
+                    <div :class="mode === light ? 'border-brand-black-3 hover:border-white hover:bg-brand-black-3 hover:text-white bg:border-brand-black-3' : 'border-white hover:border-brand-black-3 hover:bg-white hover:text-brand-black-1'"
                     class="mb-8 md:mb-0 px-8 py-5 text-center font-bold rounded-sm bg-transparent border transition-all">Send
                     an
                     Email
                     </div>
                 </a>
                 <!-- Calendly link widget begin -->
-                <a :class="mode === light ? 'bg-brand-black-3 text-white hover:bg-white hover:text-brand-black-1' : 'bg-white text-brand-black-1 hover:border hover:border-white hover:bg-transparent hover:text-white'" class="block px-8 py-5 text-center font-bold rounded-sm cursor-pointer border transition-all"
+                <a :class="mode === light ? 'bg-brand-black-3 text-white hover:bg-white hover:text-brand-black-1 hover:border-brand-black-3' : 'bg-white text-brand-black-1 hover:border hover:border-white hover:bg-transparent hover:text-white'" class="block px-8 py-5 text-center font-bold rounded-sm cursor-pointer border transition-all"
                     onclick="Calendly.initPopupWidget({url: 'https://calendly.com/aiseidahor'});return false;">Book an
                     appointment</a>
                 <!-- Calendly link widget end -->               

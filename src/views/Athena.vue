fa108857d9813416f54681da584b848e25b9ba45
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-07-30 07:54:32 -0700

    Fixes to typos on Athena

--- src/views/Athena.vue
+++ src/views/Athena.vue
@@ -18,7 +18,7 @@ import photo6 from "../assets/athena/trade.png"
 import photo7 from "../assets/athena/launchpool.png"
 const info = {
     title: "Athena",
-    text: "Providing investment opportunities, real-time trading activities and launchpad opportunities for investing in new tokens before the are listed to users",
+    text: "Providing investment opportunities, real-time trading activities and launchpad opportunities for investing in new tokens before they are listed to users",
     img: athenaCover,
     alt: "Athena",
     mainContent: "Athena is a decentralized exchange (DEX) on the Ethereum. Athena’s Exchange design is based on automated market maker (AMM)",
@@ -31,8 +31,8 @@ const info = {
 const problems = {
     title: "Problems associated with Decentralized Exchanges",
     paragraphs: [
-        "With the fast growth of crypto and its adoption growing massively daily, which keeps us in constant search for the best opportunities, and with the lack of trust in centralized exchanges, the goal own and is in control of your crypto investments and at the same time enjoy the benefits given on centralized exchanges",
-        "With the increased adoption of Decentralized exchanges, there still remains the problem of liquidity which in turn affects the speed of transaction settlement and transparency in the elements of cost and pricing",
+        "With the fast growth of crypto and its adoption growing massively daily, which keeps us in constant search for the best opportunities, and with the lack of trust in centralized exchanges, the goal is to own and be in control of your crypto investments and at the same time enjoy the benefits given on centralized exchanges",
+        "With the increased adoption of Decentralized exchanges, there still remains the problem of liquidity which in turn affects the speed of transaction settlement and transparency in the elements cost and pricing",
         "Another setback is on participating in trading on DEXs. The ability to carry out simple selling orders or advanced functionalities for trading such as lending, stop losses, and margin trading on the majority of decentralized exchanges"
     ],
     img: photo1,
@@ -41,9 +41,9 @@ const problems = {
 const user1 = {
     img: persona1,
     alt: "Persona",
-    about: "Kenny is an 18yrs old Environmental Science student in the university of Atlanta who has been hearing of investing with Decentralized Exchanges from his friends on campus but hasnt really settled to get started because of how complex it feels",
+    about: "Kenny is an 18yrs old Environmental Science student in the university of Atlanta who has been hearing of investing with Decentralized Exchanges from his friends on campus but hasn't really settled to get started because of how complex it feels",
     goals: [
-        "Kenny needs a way to earn passive income from his crypto tokesn, so far he has a couple of stable coins just sitting in centralized exchanges and he wants a way to invest them",
+        "Kenny needs a way to earn passive income from his crypto tokens, so far he has a couple of stable coins just sitting in centralized exchanges and he wants a way to invest them",
         "He needs to be able to buy new tokens that are not listed on centralized exchanges to enjoy the benefits of early buyers",
     ],
     points: [
@@ -54,12 +54,12 @@ const user1 = {
 const user2 = {
     img: persona2,
     alt: "Persona",
-    about: "Paul is a 32yrs old biologist that lives in Australia. During the day and a crypto trader at night. he does a lot of Futures trading on binance and has been investing in cryptocurrencies since 2018, he is a sucker for new tokens becasue he has made his most profits",
+    about: "Paul is a 32yrs old biologist during the day and a crypto trader at night that lives in Australia. He does a lot of Futures trading on binance and has been investing in cryptocurrencies since 2018, he is a sucker for new tokens becasue he has made his most profits",
     goals: [
         "Paul needs a way to get hold on new tokens before they are listed",
         "Paul needs a way to monitor his investments easily",
         "Paul needs a way to stay up to date on news in the crypto ecosystem as it helps him make better trading predictions",
-        "Paul needs a way to trade with at least the basics of open orders and setting of stop limits on a decentralized exchange as the have more tokens that can be traded"
+        "Paul needs a way to trade with at least the basics of open orders and setting of stop limits on a decentralized exchange as they have more tokens that can be traded"
     ],
     points: [
         "Slow transaction time",
@@ -70,7 +70,7 @@ const user2 = {
 const dashboard = {
     title: "Dashboard Overview",
     paragraphs: [
-        "The dashboard overview was designed to solve the issues with monitoring your transactions and your earnings. With the dashboard overview as well, you have access to the trending news today in the crypto space to help you stay up to date on crypto events and also et to see the top Gainers and the top losers in the market and use these data to guide your trading decisions. You also have access to the protocols statistics in data like their market-cap, price, current index, etc to use to validate the token of the protocol and also invest in them"
+        "The dashboard overview was designed to solve the issues with monitoring your transactions and your earnings. With the dashboard overview as well, you have access to the trending news today in the crypto space to help you stay up to date on crypto events. Also access to see the top Gainers and the top Losers in the market and use these data to guide your trading decisions. You also have access to the protocols statistics in data like their market-cap, price, current index, etc to use to validate the token of the protocol and also invest in them"
     ],
     img: photo2,
     alt: "Dashboard"
@@ -78,9 +78,9 @@ const dashboard = {
 const swapBridge = {
     title: "Swap & Bridge",
     paragraphs: [
-        "A simplified way to send converts crypto from one asset to another on the same blockchain (Swap) or different blockchains (Bridge)",
+        "A simplified way to send and convert crypto from one asset to another on the same blockchain (Swap) or different blockchains (Bridge)",
         "Initially, I designed to merge the two into one action since they both have the same purpose but perform similar but slightly different actions as the bridge sent between multiple tokens in which you can select a sending token and a receiving token (the same or different) and can be on the same or different blockchain",
-        "The decision to keep them separately is due to the fact that a lot of users kept searching for the bridge in AB testing so we decided to separate for now and merge them later in future versions"
+        "The decision to keep them separately is due to the fact that a lot of users kept searching for the bridge in AB testing so we decided to separate it for now and merge them later in future versions"
     ],
     img: photo3,
     alt: "Swap and Bridge"
@@ -88,8 +88,8 @@ const swapBridge = {
 const pools = {
     title: "Pools",
     paragraphs: [
-        "The pools in Athena are a collection of assets of various pairs in which you could add liquidity or remove the liquidity from the home page without having to go into to see more details, in which the major details that are needed by the user to decide on what pool to invest in has been displayed already so this is to reduce the number of clicks before successfully investing in a pool",
-        "Also, you have full control of the remove liquidity section, where you can either  control the percentage with a slider or you signify the amount of LP you want to remove or the number of a particular asset "
+        "The pools in Athena are a collection of assets of various pairs in which you could add liquidity or remove the liquidity from the home page without having to go into it to see more details, in which the major details that are needed by the user to decide on what pool to invest in has been displayed already. This is to reduce the number of clicks before successfully investing in a pool",
+        "Also, you have full control of the remove liquidity section, where you can either control the percentage with a slider or you signify the amount of LP you want to remove or the number of a particular asset "
     ],
     img: photo4,
     alt: "Pools"
@@ -98,7 +98,7 @@ const farms = {
     title: "Farms",
     paragraphs: [
         "The farm gives you the ability to invest in the farming opportunities of the protocol with your LP tokens",
-        "We would take out of the LP tokens you have staked in pools in the platform or from any pool you have invested in other protocols and we use that LP to invest in the Farm and you earn the native token of the protocol, and all this can be done directly from the dashboard"
+        "We would take out of the LP tokens you have staked in pools in the platform or from any pool you have invested in and we use that LP to invest in the Farm and you earn the native token of the protocol, and all this can be done directly from the dashboard"
     ],
     img: photo5,
     alt: "Farms"
@@ -106,8 +106,8 @@ const farms = {
 const funding = {
     title: "Futures Trading Portal",
     paragraphs: [
-        "The trading portal is one problem that a lot of users f the DeFi space do not have access to due to the fast-paced nature it requires steady and available liquidity",
-        "Whatever amount is traded is saved as trading funds and we support trades from pools that are currently available on the platform, whereby the funds in the pool are what would be used to facilitate the tradings from the, and the fees generated would be distributed to the LP holders, hence more money to them"
+        "The trading portal is one problem that a lot of users in the DeFi space do not have access to due to the fast-paced nature. It requires steady and available liquidity",
+        "Whatever amount is traded is saved as trading funds and we support trades from pools that are currently available on the platform, whereby the funds in the pool are what would be used to facilitate the tradings from the pool, and the fees generated would be distributed to the LP holders, hence more money to them"
     ],
     img: photo6,
     alt: "Funding"
@@ -115,13 +115,13 @@ const funding = {
 const launchpad = {
     title: "Launchpad Screen",
     paragraphs: [
-        "The Athena launchpad gives you the ability to invest in new tokens or protocols and earn the rewards of early investors. And in the crypto space, this is vital in maximizing profits. So we would be a hub for vetting new protocols that come up daily and ensure just credible protocols are listed and only holders or the ATN tokens would be able to invest in the listed protocols. Because there might be a lot of investors who might be above the tokens set by the protocol listed, we would split the tokens equally based on their investment strength",
+        "The Athena launchpad gives you the ability to invest in new tokens or protocols and earn the rewards of early investors. And in the crypto space, this is vital in maximizing profits. So we would be a hub for vetting new protocols that come up daily and ensure just credible protocols are listed and only holders on the ATN tokens would be able to invest in the listed protocols. Because there might be a lot of investors who might be above the tokens set by the protocol listed, we would split the tokens equally based on their investment strength",
     ],
 }
 const launchpool = {
     title: "Launchpool Screens",
     paragraphs: [
-        "Athena Launchpool is a platform that lets users stake ATN to earn newly listed tokens for free. The number of tokens you earn is relative to the number of tokens you stake into the pool. Launchpool users stake their $ATN tokens into the project staking pools. The AME runs for a week to allow everyone to “mine” their allocation. The deposit window opens for payment of your mined allocation in $ATN. Users at the end of the staking period which is approximately 30 days would be given back their tokens and the new token as a reward, which helps them become early investors in a new, promising, and vetted project and also diversify their portfolio.",
+        "Athena Launchpool is a platform that lets users stake ATN to earn newly listed tokens for free. The number of tokens you earn is relative to the number of tokens you stake into the pool. Launchpool users stake their ATN tokens into the project staking pools. The AME runs for a week to allow everyone to “mine” their allocation. The deposit window opens for payment of your mined allocation in $ATN. Users at the end of the staking period which is approximately 30 days would be given back their tokens and the new token as a reward, which helps them become early investors in a new, promising, and vetted project and also diversify their portfolio.",
     ],
     img: photo7,
     alt: "Launchpool"
@@ -145,10 +145,8 @@ const themeMode = 'light'
                             with a simplified experience to remove the complexity attached to a DEX.</p>
                         <p>With the growing need for a more stable or larger liquidity volume to facilitate a better
                             transaction between token pairs and other investment opportunities, there are some
-                            limitations
-                            that Decentralized exchanges so far fail to achieve and this has prevented users from
-                            optimizing
-                            their experience with regard;</p>
+                            limitations that Decentralized exchanges so far fail to achieve and this has prevented users from
+                            optimizing their experience with regard;</p>
                         <ul class="pl-6 list-disc">
                             <li>Cheaper trading fees.</li>
                             <li> Faster Trading Time.</li>
@@ -162,13 +160,12 @@ const themeMode = 'light'
         <section class="max-w-7xl mx-auto my-20 md:my-40 px-6">
             <h1 class="mb-6 text-4xl md:text-6xl font-bold font-new-paris-kingsize">Research Began</h1>
             <p class="mb-4">After listening carefully to stakeholders and the product owners, since they are the first
-                source of information source as they were the ones with the project idea and brief and have to an extent
+                source of information as they were the ones with the project idea and brief and have to an extent
                 gotten down a bit of research of their own, I began with the UX phase which started with series of
                 intense research.</p>
             <p class="mb-4">I went ahead to speak with a couple of crypto investors and interested investors, there were
-                some
-                differences found in the pain points faced by the users with each category of users having similar
-                issues while the other category having another. But there was one major similarity we learned they all
+                some differences found in the pain points faced by the users with each category of users having similar
+                issues while the other category having another. But there was one major similarity, we learned they all
                 struggle with a fundamental issue: faster transactions.</p>
             <p class="mb-4">In this process, user's needs were identified and understood in an in-depth manner, and from
                 this personas were
@@ -202,8 +199,7 @@ const themeMode = 'light'
                 <li class="mb-3">A couple of users prefer to keep their money in popular coins like Ethereum and Bitcoin
                 </li>
                 <li>Some users feel shy to ask their questions in the communities like Discord which is widely used by
-                    most
-                    protocols</li>
+                    most protocols</li>
             </ul>
         </section>
         <section class="max-w-7xl mx-auto my-20 md:my-40 px-6">
@@ -227,7 +223,7 @@ const themeMode = 'light'
             <h1 class="mb-6 text-4xl md:text-6xl font-bold font-new-paris-kingsize">Value Proposition - Customers</h1>
             <p class="mb-10 text-base">In the customer's profile, you describe the job your customer is trying to get
                 done by
-                highlighting customers pains and gains, it's used to track users needs and position the company’s to
+                highlighting customers pains and gains. It's used to track users needs and position the company’s to
                 know more about the customers needs to properly create value</p>
             <div><img class="w-full" src="../assets/athena/value-proposition-customers.png" alt="Value Proposition" /></div>
         </section>
@@ -249,11 +245,10 @@ const themeMode = 'light'
                 <div class="my-20 md:my-40">
                     <h1 class="mb-6 text-4xl md:text-6xl font-bold font-new-paris-kingsize">Design Decisions</h1>
                     <p class="mb-4">From the research carried out, the data analysis from responses, and brainstorming
-                        session with those analyses, a design was carried out t properly account for every problem and
+                        session with those analyses, a design was carried out to properly account for every problem and
                         pain
-                        points faced by the users in an inclusive manner and the design was split into two aspects both
-                        can
-                        stand as individuals products but merged into one to improve value to users</p>
+                        points faced by the users in an inclusive manner and the design was split into two aspects so both
+                        can stand as individuals products but merged into one to improve value to users</p>
                     <div class="flex items-center space-x-10">
                         <div class="flex items-center space-x-4">
                             <div class="w-2 h-2 rounded-full bg-white"></div>
